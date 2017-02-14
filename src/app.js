6b3a0ca2c9e491d222cfc1efb775af7fa54645a0
developersworkspace@gmail.com <developersworkspace@gmail.com>
2017-02-14 17:37:15 +0200

    Adds commander

--- src/app.js
+++ src/app.js
@@ -1,75 +1,101 @@
+// node app.js -p hello -d "F:\Temp\Source" "F:\Temp\Destination"
+// node app.js -p hello -d "F:\Temp\Destination" "F:\Temp\Source"
+
 var crypto = require('crypto');
 var recursive = require('recursive-readdir');
 var fs = require('fs');
 var zlib = require('zlib');
 var path = require('path');
-
-var algorithm = 'aes-256-ctr';
-var password = 'd6F3Efeq';
-
-let sourceDirectory = 'F:/Temp/Source';
-let destinationDirectory = 'F:/Temp/Destination';
-
-recursive(sourceDirectory, function (err, files) {
-    for (var i = 0; i < files.length; i++) {
-        let relativePath = path.relative(sourceDirectory, files[i]);
-        let directoryRelativePath = path.dirname(relativePath);
-
-        createDirectory(path.join(destinationDirectory, directoryRelativePath));
-        encryptFile(files[i], path.join(destinationDirectory, relativePath));
-
-        fs.unlinkSync(files[i]);
-    }
-});
-
-// let sourceDirectory = 'F:/Temp/Destination';
-// let destinationDirectory = 'F:/Temp/Source';
-
-// recursive(sourceDirectory, function (err, files) {
-//     for (var i = 0; i < files.length; i++) {
-//         let relativePath = path.relative(sourceDirectory, files[i]);
-//         let directoryRelativePath = path.dirname(relativePath);
-
-//         createDirectory(path.join(destinationDirectory, directoryRelativePath));
-//         decryptFile(files[i], path.join(destinationDirectory, relativePath));
-
-//         fs.unlinkSync(files[i]);
-//     }
-// });
+var program = require('commander');
 
 
-//encryptFile('C:/Users/Barend.Erasmus/Desktop/Capture.PNG', 'C:/Users/Barend.Erasmus/Desktop/Capture.PNG.enc');
-//decryptFile('C:/Users/Barend.Erasmus/Desktop/Capture.PNG.enc', 'C:/Users/Barend.Erasmus/Desktop/out.PNG');
+program
+    .arguments('<source> <destination>')
+    .option('-p, --password <password>', 'The AES password')
+    .option('-e, --encrypt', 'Encrypt')
+    .option('-d, --decrypt', 'Decrypt')
+    .option('-c, --clean', 'Deletes files after encrypt/decrypt')
+    .action(function (sourceDirectory, destinationDirectory) {
+        if (program.encrypt == true) {
+            encryptDirectory(sourceDirectory, destinationDirectory, program.password, program.clean);
+        } else if (program.decrypt == true) {
+            decryptDirectory(sourceDirectory, destinationDirectory, program.password, program.clean);
+        }
+    })
+    .parse(process.argv);
 
-function encryptFile(sourceFilePath, destinationFilePath) {
-    var inputStream = fs.createReadStream(sourceFilePath);
-    var outputStream = fs.createWriteStream(destinationFilePath);
 
-    var zip = zlib.createGzip();
-    var unzip = zlib.createGunzip();
+function encryptDirectory(sourceDirectory, destinationDirectory, password, clean) {
 
-    var encrypt = crypto.createCipher(algorithm, password);
-    var decrypt = crypto.createDecipher(algorithm, password)
+    recursive(sourceDirectory, function (err, files) {
+        for (let i = 0; i < files.length; i++) {
+            let relativePath = path.relative(sourceDirectory, files[i]);
+            let directoryRelativePath = path.dirname(relativePath);
+
+            createDirectory(path.join(destinationDirectory, directoryRelativePath));
+            encryptFile(files[i], path.join(destinationDirectory, relativePath), password);
+
+            if (clean == true) {
+                fs.unlinkSync(files[i]);
+            }
+        }
+    });
+}
+
+function decryptDirectory(sourceDirectory, destinationDirectory, password, clean) {
+
+    recursive(sourceDirectory, function (err, files) {
+        for (let i = 0; i < files.length; i++) {
+            let relativePath = path.relative(sourceDirectory, files[i]);
+            let directoryRelativePath = path.dirname(relativePath);
+
+            createDirectory(path.join(destinationDirectory, directoryRelativePath));
+            decryptFile(files[i], path.join(destinationDirectory, relativePath), password);
+
+            if (clean == true) {
+                fs.unlinkSync(files[i]);
+            }
+        }
+    });
+}
+
+
+
+function encryptFile(sourceFilePath, destinationFilePath, password) {
+
+    let algorithm = 'aes-256-ctr';
+
+    let inputStream = fs.createReadStream(sourceFilePath);
+    let outputStream = fs.createWriteStream(destinationFilePath);
+
+    let zip = zlib.createGzip();
+    let unzip = zlib.createGunzip();
+
+    let encrypt = crypto.createCipher(algorithm, password);
+    let decrypt = crypto.createDecipher(algorithm, password)
 
     inputStream.pipe(zip).pipe(encrypt).pipe(outputStream);
 }
 
-function decryptFile(sourceFilePath, destinationFilePath) {
-    var inputStream = fs.createReadStream(sourceFilePath);
-    var outputStream = fs.createWriteStream(destinationFilePath);
+function decryptFile(sourceFilePath, destinationFilePath, password) {
 
-    var zip = zlib.createGzip();
-    var unzip = zlib.createGunzip();
+    let algorithm = 'aes-256-ctr';
 
-    var encrypt = crypto.createCipher(algorithm, password);
-    var decrypt = crypto.createDecipher(algorithm, password)
+    let inputStream = fs.createReadStream(sourceFilePath);
+    let outputStream = fs.createWriteStream(destinationFilePath);
+
+    let zip = zlib.createGzip();
+    let unzip = zlib.createGunzip();
+
+    let encrypt = crypto.createCipher(algorithm, password);
+    let decrypt = crypto.createDecipher(algorithm, password)
 
     inputStream.pipe(decrypt).pipe(unzip).pipe(outputStream);
 }
 
 function createDirectory(dirpath) {
-    var parts = dirpath.split(path.sep);
-    for (var i = 1; i <= parts.length; i++) {
+    let parts = dirpath.split(path.sep);
+    for (let i = 1; i <= parts.length; i++) {
         mkdirSync(path.join.apply(null, parts.slice(0, i)));
     }
 }
